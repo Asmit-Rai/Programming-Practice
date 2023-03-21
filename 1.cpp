866098d6787b5dc09c1cd75d71a6b7e1b2a2e2bc
Asmit-Rai <Er.asmirai@gmail.com>
2023-03-22 00:18:14 +0530

    first c++ program

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=0;
+    int ques;
+    cout<<"enter the question solved per weeks"<<endl;
+    for (i=0;i<=4;i++)
+    {
+       cin>>ques;
+    }
+    
+    
+}
