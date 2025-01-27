f35ffa0d82d85781da16863a1105d94de1ce697a
Asmit-Rai <211b075@juetguna.co.in>
2025-01-27 15:18:43 +0530

    Print_Number_Using_Recursion.cpp

---
+++ Data Structure/Recursion/Print_Number_Using_Recursion.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+    while(n!=0)
+    {
+        cout<<n<<endl;
+        n--;
+    }
+}
+int main()
+{
+    print(5);
+    return 0;
+}
